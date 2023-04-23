75a466ddc36197f90d5dba261ddc3e6b3afa73a9
Leila <badeeb9611@gmail.com>
2023-04-23 16:54:09 +0200

    update

--- income.py
+++ income.py
@@ -1,12 +1,14 @@
-from flask import Flask
-from flask import send_file
-app = Flask(__name__)
-
+# type: ignore
 import seaborn as sns
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+
+
+from flask import Flask
+from flask import send_file
+app = Flask(__name__)
 
 # prevent matplotlib from opening a gui
 plt.ioff()
@@ -15,10 +17,13 @@ mpl.use('Agg')
 
 @app.route('/')
 def hello_world():
-    data_source = np.random.randint(1, 6, 40).reshape(10, 4)
-    # your code goes here....
-    df = pd.DataFrame(data_source)
-    df.columns = ['Class A', 'Class B', 'Class C', 'Class D']
+    with open('income.txt', 'r') as f:
+        lines = f.readlines()
+        header = lines[0].strip().split(',')
+        data_source = [list(map(int, line.strip().replace(' ', '').split(','))) for line in lines[1:]]
+ 
+    
+    df = pd.DataFrame(data_source, columns=header)
     sns_plot = sns.barplot(palette="ch:.25", data=df, ci=None)
     sns_plot.figure.savefig("output.png")
     plt.close()
@@ -28,3 +33,9 @@ def hello_world():
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
+    
+
+
+
+
+
